2b15e832f8178099908c96cea8b508ffc6e728ef
Paulus Schoutsen <balloob@gmail.com>
2021-06-09 21:50:29 -0700

    Use typescript plugin instead of tsc

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
 
 const config = {
-  input: "dist/index.js",
+  input: "src/index.ts",
   output: {
     dir: "esphome_frontend/static/js/esphome/",
     format: "module",
   },
   preserveEntrySignatures: false,
-  plugins: [nodeResolve(), json()],
+  plugins: [typescript(), nodeResolve(), json()],
 };
 
 if (process.env.NODE_ENV === "production") {
