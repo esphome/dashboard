be5147141517c3fb82885ff7774aaae2bd59e799
Paulus Schoutsen <balloob@gmail.com>
2021-06-14 21:30:38 -0700

    Version bump to 20210614.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esphome-dashboard",
-    version="20210611.0",
+    version="20210614.0",
     description="ESPHome dashboard",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
