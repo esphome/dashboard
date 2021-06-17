6f37c8e0652759da7cb8d0995381727978ca0bca
Paulus Schoutsen <balloob@gmail.com>
2021-06-17 11:06:16 -0700

    Hash entrypoint to avoid caching

--- raw_package/__init__.py
+++ raw_package/__init__.py
@@ -1,7 +1,12 @@
 """ESPHome dashboard."""
 from pathlib import Path
+import json
 
 
 def where():
     """Return path to the frontend."""
     return Path(__file__).parent
+
+def entrypoint():
+    manifest = json.loads((where() / "static/js/esphome/manifest.json").read_text())
+    return manifest['index']
