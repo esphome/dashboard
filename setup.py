28668aa3a59f40a03dcaeaca6d7894101dd972cd
Paulus Schoutsen <paulus@home-assistant.io>
2021-06-23 15:11:46 -0700

    20210623.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esphome-dashboard",
-    version="20210622.0",
+    version="20210623.0",
     description="ESPHome dashboard",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
