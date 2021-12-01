90e3d6d3d8e2c5bc8bf6f852654d5388db6de54b
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2021-12-01 13:49:24 +1300

    Bump version to 20211201.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esphome-dashboard",
-    version="20211021.1",
+    version="20211201.0",
     description="ESPHome dashboard",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
