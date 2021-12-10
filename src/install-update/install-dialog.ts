fd8d00eb022a6092b34e602589f1be66403394ae
Paulus Schoutsen <balloob@gmail.com>
2021-12-09 22:50:02 -0800

    Allow showing logs over webserial

--- src/install-update/install-dialog.ts
+++ src/install-update/install-dialog.ts
@@ -1,14 +1,7 @@
 // On pick and it's OTA or serial on host
 // Then set window.SELECTED_UPLOAD_PORT to a string.
 
-import {
-  LitElement,
-  html,
-  PropertyValues,
-  css,
-  svg,
-  TemplateResult,
-} from "lit";
+import { LitElement, html, PropertyValues, css, TemplateResult } from "lit";
 import { customElement, state } from "lit/decorators.js";
 import { getSerialPorts, SerialPort } from "../api/serial-ports";
 import "@material/mwc-dialog";
@@ -16,7 +9,12 @@ import "@material/mwc-list/mwc-list-item.js";
 import "@material/mwc-circular-progress";
 import "@material/mwc-button";
 import { connect, ESPLoader } from "esp-web-flasher";
-import { allowsWebSerial, metaChevronRight, supportsWebSerial } from "../const";
+import {
+  allowsWebSerial,
+  metaChevronRight,
+  metaHelp,
+  supportsWebSerial,
+} from "../const";
 import {
   compileConfiguration,
   Configuration,
@@ -28,12 +26,6 @@ import { openCompileDialog } from "../compile";
 
 const OK_ICON = "🎉";
 const WARNING_ICON = "👀";
-
-const metaHelp = svg`
-  <svg width="24" height="24" viewBox="0 0 24 24" slot="meta">
-    <path d="M11,18H13V16H11V18M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,6A4,4 0 0,0 8,10H10A2,2 0 0,1 12,8A2,2 0 0,1 14,10C14,12 11,11.75 11,15H13C13,12.75 16,12.5 16,10A4,4 0 0,0 12,6Z" />
-  </svg>
-`;
 
 @customElement("esphome-install-dialog")
 class ESPHomeInstallDialog extends LitElement {
---
+++ src/logs-webserial/index.ts
@@ -0,0 +1,12 @@
+const preload = () => import("./logs-webserial-dialog");
+
+export const openLogsWebSerialDialog = (
+  configuration: string,
+  port: SerialPort
+) => {
+  preload();
+  const dialog = document.createElement("esphome-logs-webserial-dialog");
+  dialog.configuration = configuration;
+  dialog.port = port;
+  document.body.append(dialog);
+};
