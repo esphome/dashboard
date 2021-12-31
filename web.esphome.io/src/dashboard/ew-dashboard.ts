6e0f0582e061d69cbbd27ea683570f6f33991372
Paulus Schoutsen <balloob@gmail.com>
2021-12-30 20:37:49 -0800

    Fix CSS

--- web.esphome.io/src/dashboard/ew-dashboard.ts
+++ web.esphome.io/src/dashboard/ew-dashboard.ts
@@ -9,40 +9,42 @@ import { supportsWebSerial } from "../../../src/const";
 class EWDashboard extends LitElement {
   protected render() {
     return html`
-      ${supportsWebSerial
-        ? html`<ew-connect-card></ew-connect-card>`
-        : html`<ew-unsupported-card></ew-unsupported-card>`}
-      <div class="intro">
-        <div class="arrow">
-          <svg width="48" height="48" viewBox="0 0 24 24">
-            <path
-              d="M20 18V20H13.5C9.91 20 7 17.09 7 13.5V7.83L3.91 10.92L2.5 9.5L8 4L13.5 9.5L12.09 10.91L9 7.83V13.5C9 16 11 18 13.5 18H20Z"
-            />
-          </svg>
-        </div>
-        <div class="text">
-          <p><b>Welcome to ESPHome Web!</b></p>
-          <p>
-            ESPHome Web allows you to check the logs of your ESP device and
-            install new versions directly from your browser.
-          </p>
-          <p>
-            This page is a lite variant of ESPHome. If you want to create and
-            edit ESPHome projects, install ESPHome on your computer or inside
-            Home&nbsp;Assistant (it's free!).
-          </p>
-          <p>
-            <a href="https://esphome.io/guides/getting_started_hassio.html"
-              >Get ESPHome</a
-            >
-          </p>
+      <div class="container">
+        ${supportsWebSerial
+          ? html`<ew-connect-card></ew-connect-card>`
+          : html`<ew-unsupported-card></ew-unsupported-card>`}
+        <div class="intro">
+          <div class="arrow">
+            <svg width="48" height="48" viewBox="0 0 24 24">
+              <path
+                d="M20 18V20H13.5C9.91 20 7 17.09 7 13.5V7.83L3.91 10.92L2.5 9.5L8 4L13.5 9.5L12.09 10.91L9 7.83V13.5C9 16 11 18 13.5 18H20Z"
+              />
+            </svg>
+          </div>
+          <div class="text">
+            <p><b>Welcome to ESPHome Web!</b></p>
+            <p>
+              ESPHome Web allows you to check the logs of your ESP device and
+              install new versions directly from your browser.
+            </p>
+            <p>
+              This page is a lite variant of ESPHome. If you want to create and
+              edit ESPHome projects, install ESPHome on your computer or inside
+              Home&nbsp;Assistant (it's free!).
+            </p>
+            <p>
+              <a href="https://esphome.io/guides/getting_started_hassio.html"
+                >Get ESPHome</a
+              >
+            </p>
+          </div>
         </div>
       </div>
     `;
   }
 
   static styles = css`
-    :host {
+    .container {
       display: flex;
       flex-direction: column;
       margin: 20px auto;
@@ -53,7 +55,7 @@ class EWDashboard extends LitElement {
     a {
       color: var(--mdc-theme-primary);
     }
-    :host > * {
+    .container > * {
       display: block;
       box-sizing: border-box;
       width: 100%;
