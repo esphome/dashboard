a1ae89228459d6d272bf714487def079ff62071e
Paulus Schoutsen <paulus@home-assistant.io>
2021-12-30 20:36:50 -0800

    Make install-web dialog reusable (#168)

--- src/install-web/index.ts
+++ src/install-web/index.ts
@@ -1,14 +1,31 @@
-import type { ESPLoader } from "esp-web-flasher";
+import { connect, ESPLoader } from "esp-web-flasher";
+import type { ESPHomeInstallWebDialog } from "./install-web-dialog";
 
 const preload = () => import("./install-web-dialog");
 
-export const openInstallWebDialog = (
-  configuration: string,
-  esploader: ESPLoader
-) => {
+export const openInstallWebDialog = async (
+  params: ESPHomeInstallWebDialog["params"]
+): Promise<boolean> => {
   preload();
+
+  let esploader: ESPLoader;
+
+  if (params.port) {
+    esploader = new ESPLoader(params.port, console);
+  } else {
+    try {
+      esploader = await connect(console);
+    } catch (err: any) {
+      if ((err as DOMException).name !== "NotFoundError") {
+        alert(`Unable to connect: ${err.message}`);
+      }
+      return false;
+    }
+  }
+
   const dialog = document.createElement("esphome-install-web-dialog");
-  dialog.configuration = configuration;
+  dialog.params = params;
   dialog.esploader = esploader;
   document.body.append(dialog);
+  return true;
 };
