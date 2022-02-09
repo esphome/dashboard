dbba0a8bbe19e91b9931ecbedd2d60d15abf0759
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-02-09 23:22:13 +1300

    Bump version to 20220209.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esphome-dashboard",
-    version="20220116.0",
+    version="20220209.0",
     description="ESPHome dashboard",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
