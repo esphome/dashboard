d7e3234ff5ff8b8adb5c0393e080efcd65abea35
Otto Winter <otto@otto-winter.com>
2022-02-19 15:52:06 +0100

    Bump version to 20220219.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esphome-dashboard",
-    version="20220209.0",
+    version="20220219.0",
     description="ESPHome dashboard",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
