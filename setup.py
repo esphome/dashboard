6ef624aeec94fd1fa89a27e5ed4ba016267dc063
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-03-09 09:48:38 +1300

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esphome-dashboard",
-    version="20220309.0,
+    version="20220309.0",
     description="ESPHome dashboard",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
