500fe9ec493ec4db0b0e69d408499383eadd285f
Paulus Schoutsen <balloob@gmail.com>
2022-09-19 23:18:21 -0400

    20220919.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="esphome-dashboard",
-    version="20220919.0",
+    version="20220919.1",
     description="ESPHome dashboard",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
