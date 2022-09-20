7f8632bd847725f2c2961660b73079e74801aeb5
Paulus Schoutsen <balloob@gmail.com>
2022-09-20 10:47:05 -0400

    Revert "Add a download dialog (#287)" and add download YAML menu option (#296)

--- src/download-choose/index.ts
+++
@@ -1,8 +0,0 @@
-const preload = () => import("./download-choose-dialog");
-
-export const openDownloadChooseDialog = (configuration: string) => {
-  preload();
-  const dialog = document.createElement("esphome-download-choose-dialog");
-  dialog.configuration = configuration;
-  document.body.append(dialog);
-};
