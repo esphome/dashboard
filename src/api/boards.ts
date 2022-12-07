3046fbedfb33e84eacdef135bf7ce325358d7c7d
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2022-12-07 16:18:49 +1300

    Fetch boards list from backend (#376)

---
+++ src/api/boards.ts
@@ -0,0 +1,10 @@
+import { fetchApiJson } from ".";
+
+export interface SupportedBoards {
+  esp32: { [key: string]: string };
+  esp8266: { [key: string]: string };
+  rp2040: { [key: string]: string };
+}
+
+export const getSupportedBoards = () =>
+  fetchApiJson<SupportedBoards>("./boards");
