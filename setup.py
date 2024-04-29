ee2e88dba10904cd7b026f8748b6c4435214bcea
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-04-29 20:14:38 +1200

    Bump version to 20240429.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as readme:
 
 setup(
     name="esphome-dashboard",
-    version="20240429.0",
+    version="20240429.1",
     description="ESPHome dashboard",
     long_description=long_description,
     long_description_content_type="text/markdown",
