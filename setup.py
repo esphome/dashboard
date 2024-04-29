bce29086be40a05a2d4ed8f83b6e7b81c664aead
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-04-29 20:14:07 +1200

    Use readme file as package long description (#615)

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
+"""esphome-dashboard setup script."""
+
+import os
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as readme:
+    long_description = readme.read()
 
 setup(
     name="esphome-dashboard",
     version="20240429.0",
     description="ESPHome dashboard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/esphome/dashboard",
     author="Nabu Casa",
     author_email="hello@nabucasa.com",
