f09fe6aa03daf1a35820b10f3febed1a76ef023b
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2025-04-20 12:28:58 +0200

    add platforms: `ESP32-C2`, `ESP32-C5`, `ESP32-C61`, `ESP32-H2`. `ESP32-P4` (#711)

--- src/const.ts
+++ src/const.ts
@@ -8,8 +8,13 @@ export type SupportedPlatforms =
   | "ESP32"
   | "ESP32S2"
   | "ESP32S3"
+  | "ESP32C2"
   | "ESP32C3"
+  | "ESP32C5"
   | "ESP32C6"
+  | "ESP32C61"
+  | "ESP32H2"
+  | "ESP32P4"
   | "RP2040"
   | "BK72XX"
   | "RTL87XX";
@@ -41,17 +46,47 @@ export const supportedPlatforms = {
     showInDeviceTypePicker: true,
     defaultBoard: "esp32-s3-devkitc-1",
   },
+  ESP32C2: {
+    label: "ESP32-C2",
+    showInPickerTitle: true,
+    showInDeviceTypePicker: false,
+    defaultBoard: "esp32-c2-devkitm-1",
+  },
   ESP32C3: {
     label: "ESP32-C3",
     showInPickerTitle: true,
     showInDeviceTypePicker: true,
     defaultBoard: "esp32-c3-devkitm-1",
   },
+  ESP32C5: {
+    label: "ESP32-C5",
+    showInPickerTitle: true,
+    showInDeviceTypePicker: false,
+    defaultBoard: "esp32-c5-devkitc-1",
+  },
   ESP32C6: {
     label: "ESP32-C6",
     showInPickerTitle: true,
-    showInDeviceTypePicker: false,
+    showInDeviceTypePicker: true,
     defaultBoard: "esp32-c6-devkitc-1",
+  },
+  ESP32C61: {
+    label: "ESP32-C61",
+    showInPickerTitle: true,
+    showInDeviceTypePicker: false,
+    defaultBoard: "esp32-c61-devkitc-1",
+  },
+  ESP32H2: {
+    label: "ESP32-H2",
+    showInPickerTitle: true,
+    showInDeviceTypePicker: false,
+    defaultBoard: "esp32-h2-devkitm-1",
+  },
+  ESP32P4: {
+    label: "ESP32-P4",
+    showInPickerTitle: true,
+    showInDeviceTypePicker: false,
+    defaultBoard: "esp32-p4-function-ev-board",
   },
   ESP8266: {
     label: "ESP8266",
@@ -84,8 +119,13 @@ export const chipFamilyToPlatform = {
   ESP32: "ESP32",
   "ESP32-S2": "ESP32S2",
   "ESP32-S3": "ESP32S3",
+  "ESP32-C2": "ESP32C2",
   "ESP32-C3": "ESP32C3",
+  "ESP32-C5": "ESP32C5",
   "ESP32-C6": "ESP32C6",
+  "ESP32-C61": "ESP32C61",
+  "ESP32-H2": "ESP32H2",
+  "ESP32-P4": "ESP32P4",
   ESP8266: "ESP8266",
 } as const satisfies { [key: string]: SupportedPlatforms };
 
