8fb89d83439b05d8488480e5030ea6a3b5cbd884
J. Nick Koston <nick@home-assistant.io>
2025-09-25 19:49:35 -0500

    fix leak

--- src/api/online-status.ts
+++ src/api/online-status.ts
@@ -4,7 +4,7 @@ import { createWebSocketCollection } from "../util/websocket-collection";
 export const getOnlineStatus = () =>
   fetchApiJson<Record<string, boolean>>("./ping");
 
-// Use WebSocket for real-time ping status updates
+// Use WebSocket for real-time dashboard events
 export const subscribeOnlineStatus = (() => {
   const collection = createWebSocketCollection<Record<string, boolean>>({
     initial_state: (_, data) => data.ping,
